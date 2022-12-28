71b4030f8eab8492efb1529feb3bf0100f138dcf
serhiimischenko <serj.roshen@gmail.com>
2022-12-28 09:44:09 +0200

    Add annotation @Beforeeach initEach().

--- src/test/java/org/example/SumCalculatorTests.java
+++ src/test/java/org/example/SumCalculatorTests.java
@@ -1,25 +1,28 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SumCalculatorTests {
 
-    public SumCalculator initEach() {
-        return new SumCalculator();
+    private SumCalculator sumCalculator;
+    @BeforeEach
+    public void initEach(){
+        sumCalculator = new SumCalculator();
     }
     @Test
     public void testThat1InputHandledCorrectly() throws InterruptedException {
-        Assertions.assertEquals(1, initEach().sum(1));
+        Assertions.assertEquals(1, sumCalculator.sum(1));
     }
     @Test
     public void testThat6InputHandledCorrectly() throws InterruptedException {
-        Assertions.assertEquals(6, initEach().sum(3));
+        Assertions.assertEquals(6, sumCalculator.sum(3));
     }
     @Test
     public void testThat0InputHandledCorrectly() {
         Assertions.assertThrows(IllegalArgumentException.class,
-                () -> initEach().sum(0)
+                () -> sumCalculator.sum(0)
         );
     }
 }
